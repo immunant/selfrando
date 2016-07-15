1304fddf91fdbabb7da034e3979250ecae8ced4b
perl <perl@immunant.com>
2016-07-15 07:24:56 +0000

    remove --quiet flag in test script

--- Tests/thttpd.sh
+++ Tests/thttpd.sh
@@ -20,7 +20,7 @@ command -v ab >/dev/null 2>&1 || { echo >&2 "Apache bench (ab) not found.  Abort
 
 cd thttpd-2.27
 ./configure --quiet --host="i686-pc-linux-gnu"
-$WRAPPER_DIR/srenv make CCOPT="--no-warn" --quiet
+$WRAPPER_DIR/srenv make CCOPT="--no-warn" 
 start-stop-daemon --start --name thttpd --quiet --exec $PWD/thttpd -- -p 8080 -l /dev/null
 ab -d -q -n 10000 -c 10 http://localhost:8080/
 start-stop-daemon --stop --name thttpd
