0d9ae55f6a226c46d2307dea94748e883217b014
perl <perl@immunant.com>
2016-07-15 08:37:18 +0000

    request trusty, not precise for Travis CI.

--- Tests/thttpd.sh
+++ Tests/thttpd.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR="$(cd "$(dirname "$0" )" && pwd)"
 WRAPPER_DIR=$SCRIPT_DIR/../Tools/Wrappers/GCC
 WORK_DIR=`mktemp -d` && cd $WORK_DIR
 
@@ -16,11 +16,13 @@ trap cleanup EXIT
 curl http://acme.com/software/thttpd/thttpd-2.27.tar.gz | tar xz
 
 command -v ab >/dev/null 2>&1 || { echo >&2 "Apache bench (ab) not found.  Aborting."; exit 1; }
-#ab
 
 cd thttpd-2.27
-./configure --quiet --host="i686-pc-linux-gnu"
-$WRAPPER_DIR/srenv make CCOPT="--no-warn" 
-start-stop-daemon --start --name thttpd --quiet --exec $PWD/thttpd -- -p 8080 -l /dev/null
+
+./configure --quiet --host="i686-pc-linux-gnu" ||  { echo >&2 "configure failed."; exit 1; }
+
+$WRAPPER_DIR/srenv make CCOPT="--no-warn" --quiet ||  { echo >&2 "make failed."; exit 1; }
+
+nstart-stop-daemon --start --name thttpd --quiet --exec $PWD/thttpd -- -p 8080 -l /dev/null
 ab -d -q -n 10000 -c 10 http://localhost:8080/
 start-stop-daemon --stop --name thttpd
