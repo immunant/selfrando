7b910466b11c3a70dc0a014ed75f06567492a471
Immunant tester <team@immunant.com>
2016-07-15 17:55:24 +0000

    use wrapper script when configuring thttpd

--- Tests/thttpd.sh
+++ Tests/thttpd.sh
@@ -19,10 +19,10 @@ command -v ab >/dev/null 2>&1 || { echo >&2 "Apache bench (ab) not found.  Abort
 
 cd thttpd-2.27
 
-./configure --quiet --host="i686-pc-linux-gnu" ||  { echo >&2 "configure failed."; exit 1; }
+$WRAPPER_DIR/srenv ./configure --quiet --host="i686-pc-linux-gnu" ||  { echo >&2 "configure failed."; exit 1; }
 
 $WRAPPER_DIR/srenv make CCOPT="--no-warn" --quiet ||  { echo >&2 "make failed."; exit 1; }
 
-nstart-stop-daemon --start --name thttpd --quiet --exec $PWD/thttpd -- -p 8080 -l /dev/null
+start-stop-daemon --start --name thttpd --quiet --exec $PWD/thttpd -- -p 8080 -l /dev/null
 ab -d -q -n 10000 -c 10 http://localhost:8080/
 start-stop-daemon --stop --name thttpd
