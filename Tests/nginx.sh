daebdc252eaeca88dc256746280bc38412390873
perl <perl@immunant.com>
2016-09-08 00:37:00 +0000

    Modified nginx test script contributed by 0t1st so it works with Travis CI.

--- Tests/nginx.sh
+++ Tests/nginx.sh
@@ -1,38 +1,44 @@
 #!/bin/bash
 # Tested on Ubuntu 14.04
 # For testing only
-# Just for fun
+# Original version by 0t1st @ github
 
 INFO='\e[1;32m'
 WARNING='\e[1;36m'
 NORMAL='\e[0;m'
-PWD=$(pwd)
-NGINX_FIRST_INSTALL=1
 
-#Default value
-BENCHMARK='Y' # Benchmark nginx after compilation and installation of new binary
-LISTEN=80 # Port to benchmark nginx, on fresh/first install, default is 80, if you replace an existing nginx, check your conf
-MAKEINSTALL='N' # On first install, where nginx is not present on your system, you have to make install
-COPYBINARY='N' # On replace install, where nginx is always present on your system, you can backup dist binary, copy new binary do a benchmark and undo the change
-RESET='Y' # Reverse change to original binary, only in replace install
-LIBRARIES='Y' # Install libraries for nginx module before compilation, libxslt ...
 
-# you can add more module and option, example :
+PWD="$(cd "$(dirname "$0" )" && pwd)"
+WRAPPER_DIR=$PWD/../Tools/Wrappers/GCC
+WORK_DIR=`mktemp -d` && cd $WORK_DIR
+PREFIX=$WORK_DIR/local/nginx
+VERSION=1.9.9
+
+# deletes the temp directory
+function cleanup {
+  rm -rf "$WORK_DIR"
+  echo "Deleted temp working directory $WORK_DIR"
+}
+
+# register cleanup function to be called on the EXIT signal
+trap cleanup EXIT
+
+# you can add more module and option, example:
 # --with-cc-opt=-g -O2 -fPIE -fstack-protector --param=ssp-buffer-size=4 -Wformat -Werror=format-security -D_FORTIFY_SOURCE=2
 # --with-ld-opt=-Wl,-Bsymbolic-functions -fPIE -pie -Wl,-z,relro -Wl,-z,now
 # --with-http_xslt_module
 # --with-http_geoip_module
-OPTIONS=" --prefix=/usr/share/nginx
-            --conf-path=/etc/nginx/nginx.conf
-            --http-log-path=/var/log/nginx/access.log
-            --error-log-path=/var/log/nginx/error.log
-            --lock-path=/var/lock/nginx.lock
-            --pid-path=/run/nginx.pid
-            --http-client-body-temp-path=/var/lib/nginx/body
-            --http-fastcgi-temp-path=/var/lib/nginx/fastcgi
-            --http-proxy-temp-path=/var/lib/nginx/proxy
-            --http-scgi-temp-path=/var/lib/nginx/scgi
-            --http-uwsgi-temp-path=/var/lib/nginx/uwsgi
+# --conf-path=/etc/nginx/nginx.conf
+# --http-log-path=/var/log/nginx/access.log
+# --error-log-path=/var/log/nginx/error.log
+# --lock-path=/var/lock/nginx.lock
+# --pid-path=/run/nginx.pid
+# --http-client-body-temp-path=/var/lib/nginx/body
+# --http-fastcgi-temp-path=/var/lib/nginx/fastcgi
+# --http-proxy-temp-path=/var/lib/nginx/proxy
+# --http-scgi-temp-path=/var/lib/nginx/scgi
+# --http-uwsgi-temp-path=/var/lib/nginx/uwsgi
+OPTIONS=" --prefix=${PREFIX}
             --with-debug
             --with-pcre-jit
             --with-ipv6
@@ -53,131 +59,40 @@ OPTIONS=" --prefix=/usr/share/nginx
             --with-mail_ssl_module
             --with-threads"
 
-if [ ! -f $PWD/selfrando/Tools/Wrappers/GCC/srenv ];then
-  git clone https://github.com/immunant/selfrando.git
-  sudo apt-get install -f scons pkg-config libelf-dev zlib1g-dev
-  cd $PWD/selfrando/
-  scons
-  cd ..
-fi
+command -v ab >/dev/null 2>&1 || { echo >&2 -e "${WARNING}Apache bench (ab) not found.${NORMAL}  Trying to install..."; apt-get install -f apache2-utils; }
 
-WRAPPER_DIR=$PWD/selfrando/Tools/Wrappers/GCC
-
-if dpkg -l nginx | egrep 'ii.*nginx' > /dev/null 2>&1; then
-  echo -e "\n${INFO}Current version of nginx:${NORMAL}"
-  nginx -v
-  VERSION=$(dpkg -s nginx | grep 'Version' | cut -d ' ' -f2 | cut -d '-' -f1)
-  echo -e '\n'
-  read -p "$(echo -e ${INFO}Enter distribution version [$VERSION] or choose a different one:${NORMAL})`echo $'\n> '`" READ
-  VERSION=${READ:-$VERSION}
-  # retrieve dist compile options
-  #OPTIONS=$(2>&1 nginx -V | xargs -n1 | grep "^--" | sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/ /g')
-  NGINX_FIRST_INSTALL=0
-else
-  echo -e '\n'
-  read -p "$(echo -e ${INFO}Enter version number you want to install:${NORMAL})`echo $'\n> '`" VERSION
-fi
-
-echo -e '\n'
-read -p "$(echo -e ${INFO}Benchmark after install ? [$BENCHMARK]${NORMAL})`echo $'\n> '`" READ
-BENCHMARK=${READ:-$BENCHMARK}
-case $BENCHMARK in
-    [yY])
-        echo -e '\n'
-        read -p "$(echo -e ${INFO}Default port to benchmark [$LISTEN]:${NORMAL})`echo $'\n> '`" READ
-        LISTEN=${READ:-$LISTEN}
-        BENCHMARK=1
-        command -v ab >/dev/null 2>&1 || { echo >&2 -e "${WARNING}Apache bench (ab) not found.${NORMAL}  Trying to install..."; apt-get install -f apache2-utils; }
-        ;;
-esac
-
-if [ ! -d $PWD/nginx-$VERSION ];then
+if [ ! -d $WORK_DIR/nginx-$VERSION ];then
   echo -e "\n${INFO}Download source code in progress...${NORMAL}"
   curl http://nginx.org/download/nginx-$VERSION.tar.gz | tar xz
 fi
 
-if [ ! -d $PWD/nginx-$VERSION ];then
+if [ ! -d $WORK_DIR/nginx-$VERSION ];then
   echo -e "\n${WARNING}Nginx source code folder not found${NORMAL}"
   exit 0
 fi
 
-cd $PWD/nginx-$VERSION
+cd $WORK_DIR/nginx-$VERSION
 
-if [ ! -f objs/nginx ];then
-  echo -e '\n'
-  read -p "$(echo -e ${INFO}Install libraries dependencies to compile nginx ? [$LIBRARIES]${NORMAL})`echo $'\n> '`" READ
-  LIBRARIES=${READ:-$LIBRARIES}
-  case $LIBRARIES in
-      [yY])
-        sudo add-apt-repository ppa:maxmind/ppa
-        sudo apt-get update
-        sudo apt-get install -f libxml2-dev libxml2 libxslt1-dev libgd-dev libperl-dev libgd2-xpm-dev geoipupdate
-        sudo ldconfig
-        ;;
-  esac
-  $WRAPPER_DIR/srenv ./configure $OPTIONS ||  { echo >&2 -e "${WARNING}configure failed.${NORMAL}"; exit 1; }
-  $WRAPPER_DIR/srenv make CCOPT="--no-warn" --quiet  ||  { echo >&2 -e "${WARNING}make failed.${NORMAL}"; exit 1; }
+$WRAPPER_DIR/srenv ./configure $OPTIONS ||  { echo >&2 -e "${WARNING}configure failed.${NORMAL}"; exit 1; }
+echo -e "\n${INFO}Compiling nginx...${NORMAL}"
+$WRAPPER_DIR/srenv make CCOPT="--no-warn" --quiet  ||  { echo >&2 -e "${WARNING}make failed.${NORMAL}"; exit 1; }
+
+echo -e "\n${INFO}Installing nginx...${NORMAL}"
+$WRAPPER_DIR/srenv make install CCOPT="--no-warn" --quiet ||  { echo >&2 -e "${WARNING}make install failed.${NORMAL}"; exit 1; }
+CONF=$PREFIX/conf/nginx.conf
+if [ ! -f $CONF ];then
+  echo -e "\n${WARNING}Nginx configuration file not found${NORMAL}"
+  exit 0
 fi
+sed --in-place -e "s/^        listen       80;$/        listen       8080;/g" $CONF
 
-if [ "$NGINX_FIRST_INSTALL" -eq 1 ];then
-  echo -e '\n'
-  read -p "$(echo -e ${INFO}Do a make install ? [$MAKEINSTALL]${NORMAL})`echo $'\n> '`" READ
-  MAKEINSTALL=${READ:-$MAKEINSTALL}
-  case $MAKEINSTALL in
-      [yY])
-          $WRAPPER_DIR/srenv make install CCOPT="--no-warn" --quiet ||  { echo >&2 -e "${WARNING}make install failed.${NORMAL}"; exit 1; }
-          #@TODO Do benchmark after init script install and service installation
-          echo "${INFO}Make install finished, install init script and test benchmark manually${NORMAL}"
-          exit 0
-          ;;
-  esac
-else
-  echo -e '\n'
-  read -p "$(echo -e ${INFO}Copy new binary in /usr/sbin ? [$COPYBINARY]${NORMAL})`echo $'\n> '`" READ
-  COPYBINARY=${READ:-$COPYBINARY}
-  case $COPYBINARY in
-      [yY])
-          echo -e "\n${INFO}Current nginx version${NORMAL}"
-          /usr/sbin/nginx -v
-          echo -e "\n${INFO}Stop nginx service${NORMAL}"
-          sudo /etc/init.d/nginx stop
-          echo -e "\n${INFO}Copy new version${NORMAL}"
-          sudo mv /usr/sbin/nginx /usr/sbin/nginx-dist
-          sudo cp objs/nginx /usr/sbin/nginx
-          sudo chmod +x /usr/sbin/nginx
-          echo -e "\n${INFO}New nginx version${NORMAL}"
-          /usr/sbin/nginx -v
-          if [ "$BENCHMARK" -eq 1 ];then
-            echo -e "\n${INFO}Restart nginx service${NORMAL}"
-            sudo /etc/init.d/nginx start
-            echo -e "\n${INFO}Start benchmark${NORMAL}"
-            ab -d -q -n 10000 -c 10 http://127.0.0.1:$LISTEN/
-          fi
-          echo -e '\n'
-          read -p "$(echo -e ${INFO}Undo nginx binary to distribution version ? [$RESET]${NORMAL})`echo $'\n> '`" READ
-          RESET=${READ:-$RESET}
-          case $RESET in
-              [yY])
-                  echo -e "\n${INFO}Stopping service${NORMAL}"
-                  sudo /etc/init.d/nginx stop
-                  echo -e "\n${INFO}Backup dist version${NORMAL}"
-                  sudo mv /usr/sbin/nginx /usr/sbin/nginx-hardened
-                  sudo mv /usr/sbin/nginx-dist /usr/sbin/nginx
-                  sudo chmod +x /usr/sbin/nginx-hardened
-                  echo -e "\n${INFO}Check current version${NORMAL}"
-                  /usr/sbin/nginx -v
-                  echo -e "\n${INFO}Restart service${NORMAL}"
-                  sudo /etc/init.d/nginx start
-                  echo -e "\n${INFO}Check process${NORMAL}"
-                  sudo ps -ef | grep nginx
-                  ;;
-          esac
-          ;;
-      *)
-          echo -e "\n${INFO}Compilation finished, nginx binary in nginx-$VERSION/objs/${NORMAL}"
-          ;;
-  esac
-fi
+echo -e "\n${INFO}Testing nginx...${NORMAL}"
 
-echo -e "\n${INFO}Quit...${NORMAL}"
+NGINX=$PREFIX/sbin/nginx
+
+start-stop-daemon --start --name nginx --quiet --exec $NGINX -- 
+ab -d -q -n 10000 -c 10 http://127.0.0.1:8080/
+start-stop-daemon --stop --name nginx
+
+echo -e "\n${INFO}PASS!${NORMAL}"
 exit 0
