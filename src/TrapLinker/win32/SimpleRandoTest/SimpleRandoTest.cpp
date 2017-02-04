04d0e8ad62bdfcc63c01142ce2010010e6a48147
Andrei Homescu <ah@immunant.com>
2017-02-03 17:23:31 -0800

    Added SimpleRandoTest test application for win32 TrapLinker

---
+++ src/TrapLinker/win32/SimpleRandoTest/SimpleRandoTest.cpp
@@ -0,0 +1,11 @@
+// SimpleRandoTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+    printf("Randomization successful!!!\n");
+    return 0;
+}
