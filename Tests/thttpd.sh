c92794820547545feb7d6191b4eea329b9f9cd35
perl <perl@immunant.com>
2017-02-04 02:30:11 -0800

    Build thttpd without srenv.

--- Tests/thttpd.sh
+++ Tests/thttpd.sh
@@ -1,14 +1,11 @@
 #! /bin/bash
 
 SCRIPT_DIR="$(cd "$(dirname "$0" )" && pwd)"
-WRAPPER_DIR=$SCRIPT_DIR/../Tools/Wrappers
-
-TRAPLINKER=$SCRIPT_DIR/../out/$(uname -p)/bin/traplinker
-if [ ! -e "$TRAPLINKER" ]; then
+SELFRANDO_BIN=$SCRIPT_DIR/../out/$(uname -p)/bin
+if [ ! -e "$SELFRANDO_BIN/traplinker" ]; then
   echo "Build selfrando before running this script"
   exit 1
 fi
-
 WORK_DIR=`mktemp -d` && cd $WORK_DIR
 
 # deletes the temp directory
@@ -24,11 +21,17 @@ curl --silent http://acme.com/software/thttpd/thttpd-2.27.tar.gz | tar xz
 
 command -v ab >/dev/null 2>&1 || { echo >&2 "Apache bench (ab) not found.  Aborting."; exit 1; }
 
+NUM_PROCS=`nproc --all`
 cd thttpd-2.27
 
-$WRAPPER_DIR/srenv ./configure --quiet --host="i686-pc-linux-gnu" ||  { echo >&2 "configure failed."; exit 1; }
+SED_EXPR="s/^CCOPT =\s*\@V_CCOPT\@$/CCOPT = \t\@V_CCOPT\@ \$(CC_ADDN_OPT)/g"
+sed --in-place -e "$SED_EXPR" Makefile.in
 
-$WRAPPER_DIR/srenv make CCOPT="-w" --quiet ||  { echo >&2 "make failed."; exit 1; }
+CFLAGS="-ffunction-sections"
+LDFLAGS="-B$SELFRANDO_BIN -Wl,-rpath,$SELFRANDO_BIN -Wl,--gc-sections" 
+CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" ./configure --quiet --host="i686-pc-linux-gnu" 
+
+make CCOPT="-w" --quiet -j$NUM_PROCS STATICFLAG= CC_ADDN_OPT="$CFLAGS"
 
 start-stop-daemon --start --name thttpd --quiet --exec $PWD/thttpd -- -p 8080 -l /dev/null
 ab -d -q -n 10000 -c 10 http://localhost:8080/
