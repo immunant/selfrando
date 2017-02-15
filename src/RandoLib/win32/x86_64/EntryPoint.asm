cb9ce37683f3f170c1bafd3f8acc13e5ef31dd40
Andrei Homescu <ah@immunant.com>
2017-02-14 16:27:40 -0800

    Added x86_64 versions of RandoLib files (work-in-progress)

---
+++ src/RandoLib/win32/x86_64/EntryPoint.asm
@@ -0,0 +1,72 @@
+;
+; Copyright (c) 2014-2015, The Regents of the University of California
+; Copyright (c) 2017 Immunant Inc.
+; All rights reserved.
+;
+; Redistribution and use in source and binary forms, with or without
+; modification, are permitted provided that the following conditions are met:
+;
+; * Redistributions of source code must retain the above copyright notice, this
+;   list of conditions and the following disclaimer.
+;
+; * Redistributions in binary form must reproduce the above copyright notice,
+;   this list of conditions and the following disclaimer in the documentation
+;   and/or other materials provided with the distribution.
+;
+; * Neither the name of the University of California nor the names of its
+;   contributors may be used to endorse or promote products derived from
+;   this software without specific prior written permission.
+;
+; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+; DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+; FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+; DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+; SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+; CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+; OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+; OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+;
+
+public __TRaP_RandoEntry
+extern __TRaP_RandoMain:near
+
+.data
+__TRaP_NewEntry dq 0
+
+rndentry segment byte read execute alias(".rndentr")
+; This stores the original contents of AddressOfEntryPoint from the PE optional header
+; We store it in a separate section to make it easier to patch on-disk, and also to un-map from memory
+__TRaP_OriginalEntry dd 0
+
+; New program entry point, that AddressOfEntryPoint will point to
+__TRaP_RandoEntry proc
+	; Check if we already randomized the program
+	mov rax, qword ptr [__TRaP_NewEntry]
+	test rax, rax
+	jnz have_rando
+
+	; WARNING: PatchEntry expects this to be at a fixed offset
+	; PatchEntry changes this to "PUSH 0 ; NOP" if we're not in a DLL
+	push rcx
+	lea rax, dword ptr [__TRaP_NewEntry]
+	push rax
+    mov eax, dword ptr [__TRaP_OriginalEntry]
+    push rax
+	; Push pointer to ModuleInfo structure as single parameter
+    mov rcx, rsp
+    sub rsp, 32
+	call __TRaP_RandoMain
+    add rsp, 32
+
+	; Load the new entry point from where RandoLib put it
+	mov rax, qword ptr [__TRaP_NewEntry]
+
+have_rando:
+	; FIXME: this could be useful to attackers???
+	jmp qword ptr rax
+__TRaP_RandoEntry endp
+rndentry ends
+
+end
---
+++ src/RandoLib/win32/x86_64/Relocation.cpp
@@ -0,0 +1,75 @@
+/*
+* This file is part of selfrando.
+* Copyright (c) 2015-2017 Immunant Inc.
+* For license information, see the LICENSE file
+* included with selfrando.
+*
+*/
+
+#include <OS.h>
+#include <TrapInfo.h>
+
+os::Module::Relocation::Relocation(const os::Module &mod, const TrapReloc &reloc)
+    : m_module(mod), m_orig_src_addr(mod.address_from_trap(reloc.address)),
+    m_src_addr(mod.address_from_trap(reloc.address)), m_type(reloc.type) {
+}
+
+os::BytePointer os::Module::Relocation::get_target_ptr() const {
+    // IMPORTANT: Keep RandoLib/TrapInfoCommonh.h in sync whenever a new
+    // relocation requires a symbol and/or addend.
+
+    auto at_ptr = m_src_addr.to_ptr();
+    switch (m_type) {
+    case IMAGE_REL_AMD64_ADDR64:
+        return reinterpret_cast<os::BytePointer>(*reinterpret_cast<uint64_t*>(at_ptr));
+    case IMAGE_REL_AMD64_REL32:
+        // We need to use the original address as the source here (not the diversified one)
+        // to keep in consistent with the original relocation entry (before shuffling)
+        return m_orig_src_addr.to_ptr() + sizeof(int32_t) + *reinterpret_cast<int32_t*>(at_ptr);
+    default:
+        return nullptr;
+    }
+}
+
+void os::Module::Relocation::set_target_ptr(os::BytePointer new_target) {
+    auto at_ptr = m_src_addr.to_ptr();
+    switch (m_type) {
+    case IMAGE_REL_AMD64_ADDR64:
+        *reinterpret_cast<uint64_t*>(at_ptr) = reinterpret_cast<uintptr_t>(new_target);
+        break;
+    case IMAGE_REL_AMD64_REL32:
+        // FIXME: check for overflow here???
+        *reinterpret_cast<int32_t*>(at_ptr) = static_cast<int32_t>(new_target - (at_ptr + sizeof(int32_t)));
+        break;
+    default:
+        RANDO_ASSERT(false);
+        break;
+    }
+}
+
+os::Module::Relocation::Type os::Module::Relocation::get_pointer_reloc_type() {
+    return IMAGE_REL_AMD64_REL32;
+}
+
+os::Module::Relocation::Type
+os::Module::Relocation::type_from_based(os::Module::Relocation::Type based_type) {
+    if (based_type == IMAGE_REL_BASED_ABSOLUTE)
+        return 0;
+    if (based_type == IMAGE_REL_BASED_DIR64)
+        return IMAGE_REL_AMD64_ADDR64;
+
+    API::DebugPrintf<1>("Unknown relocation type: %d\n", (int) based_type);
+    return 0;
+}
+
+void os::Module::Relocation::fixup_export_trampoline(BytePointer *export_ptr,
+                                                     const Module &module,
+                                                     os::Module::Relocation::Callback callback,
+                                                     void *callback_arg) {
+    RANDO_ASSERT(**export_ptr == 0xE9);
+    os::Module::Relocation reloc(module,
+                                 module.address_from_ptr(*export_ptr + 1),
+                                 IMAGE_REL_AMD64_REL32);
+    (*callback)(reloc, callback_arg);
+    *export_ptr += 5;
+}
