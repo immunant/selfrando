a557dbe22db336db84bd01804774af1cc393ae89
Andrei Homescu <ah@immunant.com>
2017-03-02 15:59:41 -0800

    Switched some debug output to level 2

--- src/RandoLib/RandoLib.cpp
+++ src/RandoLib/RandoLib.cpp
@@ -271,7 +271,7 @@ void ExecSectionProcessor::RemoveEmptyFunctions() {
             m_functions[cnt] = m_functions[i];
         cnt++;
     }
-    os::API::DebugPrintf<1>("Removed %d empty functions\n",
+    os::API::DebugPrintf<2>("Removed %d empty functions\n",
                             m_functions.num_funcs - cnt);
     m_functions.num_funcs = cnt;
 }
