29593fbd7e434711bed04be8c2f8d201ed1370fc
Andrei Homescu <ah@immunant.com>
2017-03-09 13:34:41 -0800

    Added placeholder content and JavaScript code to Firefox extension

--- Tools/Firefox/extension/selfrando.js
+++ Tools/Firefox/extension/selfrando.js
@@ -1,1 +1,12 @@
-alert('Selfrando extension alert!');
+Components.utils.import("resource://gre/modules/ctypes.jsm");
+
+var kernel32 = ctypes.open("kernel32.dll");
+
+var GetCurrentProcessId = kernel32.declare("GetCurrentProcessId", ctypes.winapi_abi, ctypes.uint32_t);
+
+var pid = GetCurrentProcessId();
+
+var sr_wnd = document.getElementById("selfrando-window");
+var pid_text = document.createElement("label");
+pid_text.setAttribute("value", "Firefox process PID: " + pid);
+sr_wnd.appendChild(pid_text);
