dc63cd3bd1f39b289bf8df5d2ffe3f37c6881e6b
Andrei Homescu <ah@immunant.com>
2017-06-30 00:42:45 -0700

    Cleaner awk usage and regexes in rename_sections_txtrp.sh

--- src/RandoLib/posix/rename_sections_txtrp.sh
+++ src/RandoLib/posix/rename_sections_txtrp.sh
@@ -6,7 +6,7 @@ OUTPUT=$2
 cp $1 $2
 
 # Find all sections in $OUTPUT that begin with .text or .rodata
-SECTIONS=`objdump -h $OUTPUT | awk '{ if ($2 ~ /^.text/ || $2 ~ /^.rodata/) print $2}' | sort | uniq`
+SECTIONS=`objdump -h $OUTPUT | awk '{ if ($2 ~ /^\.(text|rodata)/) print $2 }' | sort | uniq`
 
 for section in $SECTIONS; do
     objcopy --rename-section=$section=.txtrp $OUTPUT
