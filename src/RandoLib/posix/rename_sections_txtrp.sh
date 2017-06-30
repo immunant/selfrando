fd8ae834c06ca42316c65d211d5b9a01ff30e2e3
Andrei Homescu <ah@immunant.com>
2017-06-30 13:05:13 -0700

    Reference input and output file variables by name in rename_sections_txtrp.sh

--- src/RandoLib/posix/rename_sections_txtrp.sh
+++ src/RandoLib/posix/rename_sections_txtrp.sh
@@ -3,7 +3,7 @@
 INPUT=$1
 OUTPUT=$2
 
-cp $1 $2
+cp $INPUT $OUTPUT
 
 # Find all sections in $OUTPUT that begin with .text or .rodata
 SECTIONS=`objdump -h $OUTPUT | awk '{ if ($2 ~ /^\.(text|rodata)/) print $2 }' | sort | uniq`
