46da6cf4855f8f29be42138e0fe4cf46474bc339
Per Larsen <perl@immunant.com>
2017-07-10 18:20:00 -0700

    Conditionally checkout cpython

--- tests/win32/python36.ps1
+++ tests/win32/python36.ps1
@@ -1,2 +1,23 @@
+if ((Get-Command "git.exe" -ErrorAction SilentlyContinue) -eq $null) 
+{ 
+   Write-Host "Unable to find git.exe in your PATH"
+   exit 1
+}
+
+function Get-ScriptDirectory
+{
+  $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+  Split-Path $Invocation.MyCommand.Path
+}
+
+$ScriptDir = Get-ScriptDirectory
 cd $env:TEMP
-& git clone -b 3.6 https://github.com/python/cpython
+if (-not (Test-Path "cpython")) 
+{ 
+    Write-Host "checking out cpython to $env:TEMP\cpython"
+    git clone -q --branch=3.6 https://github.com/python/cpython
+} else {
+    Write-Host "cpython already checked out to $env:TEMP\cpython"
+}
+
+cd $ScriptDir
