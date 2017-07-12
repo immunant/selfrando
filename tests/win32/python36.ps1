4945d77432118a5c4a9e34f6b344fd043b1e733b
Per Larsen <perl@immunant.com>
2017-07-12 16:06:21 -0700

    Update python test script.

--- tests/win32/python36.ps1
+++ tests/win32/python36.ps1
@@ -1,8 +1,15 @@
-if ((Get-Command "git.exe" -ErrorAction SilentlyContinue) -eq $null) 
-{ 
-   Write-Host "Unable to find git.exe in your PATH"
-   exit 1
+# Check that $executable is in $env:PATH
+function Have-Executable($executable)
+{  
+    if ((Get-Command $executable -ErrorAction SilentlyContinue) -eq $null) 
+    { 
+    Write-Host "Unable to find $executable in your PATH"
+    exit 1
+    }
 }
+
+Have-Executable("git.exe")
+Have-Executable("python.exe")
 
 function Get-ScriptDirectory
 {
@@ -11,6 +18,14 @@ function Get-ScriptDirectory
 }
 
 $ScriptDir = Get-ScriptDirectory
+$SelfrandoHome = split-path -parent (split-path -parent $ScriptDir)
+# Sanity check $selfrandoHome
+if (-not (Test-Path (Join-Path -Path $SelfrandoHome -ChildPath "LICENSE")))
+{ 
+   Write-Host 'Unable to find $selfrandoHome\LICENSE'
+   exit 1
+}
+
 cd $env:TEMP
 if (-not (Test-Path "cpython")) 
 { 
@@ -21,8 +36,17 @@ if (-not (Test-Path "cpython"))
 }
 cd "$env:TEMP\cpython\PCBuild"
 
+python.exe "$SelfrandoHome\scripts\update_vcxproj.py" --inplace -i python.vcxproj
+python.exe "$SelfrandoHome\scripts\update_vcxproj.py" --inplace -i pythoncore.vcxproj
+python.exe "$SelfrandoHome\scripts\update_vcxproj.py" --inplace -i python3dll.vcxproj
+
 # build python for Windows. -e fetches exernals as needed.
 .\build.bat -e
+
+# check that the python binaries contains a .txtrp section
+# dumpbin /section:.txtrp win32\python.exe
+# dumpbin /section:.txtrp win32\python3.dll
+
 # run python testsuite. -q means quick (single) run.
 .\rt.bat -q  
 
