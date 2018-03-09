8992ae79c848761e050e66211b86f87144b1c228
Andrei Homescu <ah@immunant.com>
2018-03-08 17:36:51 -0800

    Use cmake-provided paths to objcopy&objdump in CMakeLists.txt and rename_sections_txtrp.sh instead of the system-wide ones from PATH

--- src/RandoLib/posix/rename_sections_txtrp.sh
+++ src/RandoLib/posix/rename_sections_txtrp.sh
@@ -3,11 +3,14 @@
 INPUT=$1
 OUTPUT=$2
 
+: ${OBJCOPY:=$(which objcopy)}
+: ${OBJDUMP:=$(which objdump)}
+
 cp $INPUT $OUTPUT
 
 # Find all sections in $OUTPUT that begin with .text or .rodata
-SECTIONS=`objdump -h $OUTPUT | awk '{ if ($2 ~ /^\.(text|rodata)/) print $2 }' | sort | uniq`
+SECTIONS=`$OBJDUMP -h $OUTPUT | awk '{ if ($2 ~ /^\.(text|rodata)/) print $2 }' | sort | uniq`
 
 for section in $SECTIONS; do
-    objcopy --rename-section=$section=.txtrp $OUTPUT
+    $OBJCOPY --rename-section=$section=.txtrp $OUTPUT
 done
